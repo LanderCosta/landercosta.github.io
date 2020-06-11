127068731ddae498001d0ebc73cd7da4bb079300
Lander Costa <landercosta@gmail.com>
2020-06-11 20:37:57 -0300

    Adicionando o menu mobile

--- js/main.js
+++ js/main.js
@@ -6,10 +6,12 @@
   addHamburguerAnimation();
 
   function addHamburguerAnimation(){
-    const menuBtn = document.querySelector('.menu');
+    const menuBtn = document.querySelector('.menu-hamburguer');
+    const menuMobile = document.querySelector('.menu-mobile');
     let menuOpen = false;
     menuBtn.addEventListener('click', () => {
       menuBtn.classList.toggle('open');
+      menuMobile.classList.toggle('show');
       menuOpen = !menuOpen;
     });
   }
